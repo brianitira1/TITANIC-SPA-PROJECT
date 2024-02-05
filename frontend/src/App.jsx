203cdaf45bc02e30ae42703f152a5b81c40a001e
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-02-05 14:18:53 +0000

    feat: Add bootstrap.min.css, add routing components, add additional components, remove unused code from App.jsx and remove unused CSS from index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./components/Home";
+import SuccessPage from "./components/SuccessPage";
+import AdminDashboardPage from "./components/AdminDashBoardPage";
+import ServicesPage from "./components/ServicesPage";
+import Massage from "./components/Massage";
+import Beauty from "./components/Beauty";
+import Wellness from "./components/Wellness";
+import Footer from "./components/Footer";
+
+const app = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/massage" element={<Massage />} />
+        <Route path="/beauty" element={<Beauty />} />
+        <Route path="/wellness" element={<Wellness />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default app;
