46cf94d56e24a534cd6c2844a22f23caa59ed488
brianochomba itira <brianochomba@techie.com>
2024-02-09 21:06:41 +0000

    introducing .env.local on the database to hide api keys

--- frontend/src/databases/supabase.tsx
+++ frontend/src/databases/supabase.tsx
@@ -1,13 +1,12 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js"; // Importing createClient function from supabase-js library
 
 // Define types for Supabase URL and key
-type SupabaseUrl = string;
-type SupabaseKey = string;
+type SupabaseUrl = any;
+type SupabaseKey = any;
 
 // Supabase URL and key for authentication
-const supabaseUrl: SupabaseUrl = "https://mevswyanfiamhugiovcs.supabase.co";
-const supabaseKey: SupabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ldnN3eWFuZmlhbWh1Z2lvdmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcwNTAzMDEsImV4cCI6MjAyMjYyNjMwMX0.LlHSEiP5A5Hxwmz2BW37DLolmtX3HQ1ZtbKH2jpCBOk";
+const supabaseUrl: SupabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey: SupabaseKey = process.env.SUPABASE_KEY;
 
 // Creating Supabase client instance
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
