6916b4c015320d4b1ac1787f5028af28f05d91d5
brianochomba itira <brianochomba@techie.com>
2024-02-10 07:50:14 +0000

    fixing supabase

--- frontend/src/databases/supabase.tsx
+++ frontend/src/databases/supabase.tsx
@@ -1,12 +1,12 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js"; // Importing createClient function from supabase-js library
 
 // Define types for Supabase URL and key
-type SupabaseUrl = any;
-type SupabaseKey = any;
+type SupabaseUrl = string;
+type SupabaseKey = string;
 
 // Supabase URL and key for authentication
-const supabaseUrl: SupabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey: SupabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl: SupabaseUrl = "https://mevswyanfiamhugiovcs.supabase.co";
+const supabaseKey: SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ldnN3eWFuZmlhbWh1Z2lvdmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcwNTAzMDEsImV4cCI6MjAyMjYyNjMwMX0.LlHSEiP5A5Hxwmz2BW37DLolmtX3HQ1ZtbKH2jpCBOk";
 
 // Creating Supabase client instance
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
